a8c5d21d887d39260a8a2d850ebd64d72d4c2227
Jacob Niebloom <niebloomj@gmail.com>
2022-03-03 21:58:08 -0500

    Create lat and long of shul
    Signed-off-by: Jacob Niebloom <niebloomj@gmail.com>

--- src/bl_schedule/__main__.py
+++ src/bl_schedule/__main__.py
@@ -1,11 +1,20 @@
 """Command-line interface."""
 import click
+from zmanim.util.geo_location import GeoLocation
+from zmanim.zmanim_calendar import ZmanimCalendar
+
+tzais_ops = {'degrees': 6}
 
 
 @click.command()
 @click.version_option()
 def main() -> None:
     """BL Schedule."""
+    location = GeoLocation(
+        'Baltimore, MD', 39.363840, -76.698372, 'America/New_York'
+    )
+    calendar = ZmanimCalendar(geo_location=location)
+    print(calendar.tzais(tzais_ops))
 
 
 if __name__ == "__main__":
